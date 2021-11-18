4569e6a4e89004c861821a148325397557ea2a46
Micaela Analia Jofre <mkjofre@gmail.com>
2021-11-18 15:41:32 -0300

    formulario con php

--- secciones/contacto.php
+++ secciones/contacto.php
@@ -9,5 +9,5 @@
     $header = "From: $email \r\n";
     $header .= "Content-Type: text/plain; charset=UTF-8";
     mail($recipient, $subject, $formcontent, $header) or die("Error!");
-    header("Location: index.html");
+    header("Location:  ../index.html");
 ?>
